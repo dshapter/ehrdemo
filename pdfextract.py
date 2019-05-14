841aa5ba0419352dac71a618b90a5aa068130a39
dps <dshapter@gmail.com>
2019-05-13 22:17:55 -0400

    WIP; first pass at ES integration. Need to create indicies for each EHR type.

--- pdfextract.py
+++ pdfextract.py
@@ -2,6 +2,7 @@ import re
 import argparse
 import io
 import subprocess
+
 from ehr_error import PDFParseException
 from record import Record
 
@@ -10,6 +11,7 @@ from pdfminer.pdfinterp import PDFPageInterpreter
 from pdfminer.pdfinterp import PDFResourceManager
 from pdfminer.pdfpage import PDFPage
 
+from elasticsearch import Elasticsearch
 
 def extract_text_from_pdf(pdf_path):
     resource_manager = PDFResourceManager()
@@ -42,7 +44,7 @@ def extract_text(input, pdf_extracter_cmd):
     return output.stdout.read()
 
 
-def parse_pages(page_list, text):
+def parse_pages(page_list, text, es):
     number_of_pages = len(page_list)
     if number_of_pages == 0:
         return # throw exception here
@@ -55,12 +57,12 @@ def parse_pages(page_list, text):
         match = re.search('Page\s([0-9])\sof\s([0-9])', page_text)
         if not match:
             raise PDFParseException
-        current_num_pages = 0
+
         page_number = int(match.group(1))
         encounter_pages = int(match.group(2))
         if page_number == 1:
             # this is a new encounter/record
-            records.append(Record())
+            records.append(Record(es=es))
             records[encounter_index].add_text(page_text)
             records[encounter_index].add_helper_text(text, encounter_index)
         else:
@@ -73,7 +75,16 @@ def parse_pages(page_list, text):
         record.post_process()
 
 
-if __name__ == '__main__':
+def elastic_create_index(es, settings_file='settings.json'):
+    #  TODO: name each index according to the type of EHR we are indexing
+    with open(settings_file, 'r') as settings_fd:
+        settings = settings_fd.read()
+        if not es.indices.exists('ehr_data_1'):
+            result = es.index(index='ehr_data_1', doc_type='ehr', body=settings)
+            print(result)
+
+
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', help='PDF file to parse')
     parser.add_argument('--pdf_cmd', help='command to extract PDF')
@@ -83,7 +94,12 @@ if __name__ == '__main__':
     # for token in tokens:
     #     print(token)
     page_list = extract_text_from_pdf(args.input)
-    parse_pages(page_list, text)
+    es = Elasticsearch()
+    elastic_create_index(es)
+    parse_pages(page_list, text, es)
 
+
+if __name__ == '__main__':
+    main()
 
 
